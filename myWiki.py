9795a9beffcaf664c86148c1ab0b8fa751e0ebb6
Brian <brian.michael.evans@gmail.com>
2017-04-27 21:03:15 -0700

    Add files via upload
    Minor changes

--- myWiki.py
+++ myWiki.py
@@ -7,17 +7,23 @@ def build(pagename='main'):
     if not os.path.isfile('./content/' + str(pagename)):
         return template('editcreate', pagename=pagename)
     else:
-        return static_file(pagename,root='./content/')
+        with open('./content/' + pagename, 'r') as textfile:
+            bodytext = textfile.read()
+        return template('core', pagename=pagename, bodytext=bodytext)
 
 @route('/edit/<pagename>', method='POST')
 def createedit(pagename):
+    #pagedata == string
     pagedata = request.forms.get('PageData')
-    #pagename = request.forms.get()
+    
     fo = open('./content/' + pagename, 'w')
     fo.write(pagedata)
     fo.close()
     return template('editcomplete', pagename=pagename)
 
+@route('/styles/<css>')
+def servestyle(css):
+    return static_file(css,root='./styles/')
 
 
 if __name__ == '__main__':
